e4544fb03294008b59d39d298a9bb9c14837612a
prasanthipenmadu <prasanthipl@gmail.com>
2019-07-15 11:59:51 -0400

    Could add

--- AddingMachine/evaluator.ts
+++ AddingMachine/evaluator.ts
@@ -1,3 +1,33 @@
-class Evaluator{
+import krisstack from "krisstack";
+export class Evaluator{
     
-}
+          ops  = new krisstack<String>();
+         vals = new krisstack<number>();
+    
+      constructor() { }
+        
+          run(toEvaluate:any) {
+    
+            const tokens: string[] = toEvaluate.split("");
+            for (const token of tokens) {
+                if      (token===("("))  {}            
+                else if (token===("+"))    this.ops.push(token);
+                else if (token===("-"))    this.ops.push(token);
+                else if (token===("*"))    this.ops.push(token);
+                else if (token===("/"))    this.ops.push(token);
+                else if (token===("sqrt")) this.ops.push(token);
+                else if (token===(")")) {
+                    const op = this.ops.pop();
+                   var v = this.vals.pop();
+                    if      (op===("+"))    v = this.vals.pop() + v;
+                    else if (op===("-"))    v = this.vals.pop() - v;
+                    else if (op===("*"))    v = this.vals.pop() * v;
+                    else if (op===("/"))    v = this.vals.pop() / v;
+                    else if (op===("sqrt")) v = Math.sqrt(v);
+                    this.vals.push(v);
+                }
+                else this.vals.push(Number(token));
+            }
+            return this.vals.pop();
+        }
+    }
--- AddingMachine/src/app/text-input/text-input.component.ts
+++ AddingMachine/src/app/text-input/text-input.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Evaluator} from 'evaluator';
 @Component({
   selector: 'app-text-input',
   templateUrl: './text-input.component.html',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TextInputComponent implements OnInit {
 
+  log='';
   constructor() { }
-
+  
   ngOnInit() {
   }
+  KeyDown(event:any){
+    if(event.key==='Enter'){
+      const calculator=new Evaluator();
+      this.log=this.log+event.target.value+">>"+calculator.run(event.target.value);
+    }
+  }
+  Click(event:any){
+    this.log=this.log+"hi there! Please enter a basic Calculation for Example (5+2)";
+  }
+
 
 }
